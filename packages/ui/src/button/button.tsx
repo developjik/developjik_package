2d1a70ad5a59e55102a49778abcd0cdd0a2a43a8
developjik <developjik@gmail.com>
2024-09-23 00:12:58 +0900

    refactor(ui); change ui package name from @acme/ui to @developjik/ui

--- packages/ui/src/button/button.tsx
+++ packages/ui/src/button/button.tsx
@@ -1,9 +1,9 @@
 export interface ButtonProps
   extends React.ButtonHTMLAttributes<HTMLButtonElement> {
-  children: React.ReactNode;
+  children: React.ReactNode; // 버튼 내부에 들어갈 콘텐츠
 }
 
-export function Button({ children, ...other }: ButtonProps): JSX.Element {
+function Button({ children, ...other }: ButtonProps): JSX.Element {
   return (
     <button type="button" {...other}>
       {children}
@@ -12,3 +12,5 @@ export function Button({ children, ...other }: ButtonProps): JSX.Element {
 }
 
 Button.displayName = "Button";
+
+export { Button };
--- packages/ui/tsup.config.ts
+++ packages/ui/tsup.config.ts
@@ -1,7 +1,6 @@
 import { defineConfig } from "tsup";
 
 export default defineConfig((options) => ({
-  entryPoints: ["src/button.tsx"],
   format: ["cjs", "esm"],
   dts: true,
   sourcemap: true,
